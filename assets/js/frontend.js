4e4561f2ee64081e69b15525ed077aa295ab9b78
ivanzapatarivera <falenyo@gmail.com>
2020-09-18 10:03:42 -0400

    fixed grammar on p#3

--- assets/js/frontend.js
+++ assets/js/frontend.js
@@ -7,9 +7,9 @@ var pars = [
 
     "Hi, and thank you for taking your time to read a bit about me as a developer and individual.",
 
-    "Currently, I am a Rutger's University Full Stack Flex Bootcamp web development student out of the State of New Jersey. Through this course, I've expanded my knowledge on HTML5, CSS, JavaScript (including jQuery), MongoDB, mySQL, Express, React.js, and Node.js. I have developed online and offline apps using these technologies, which are available in my portfolio.", 
+    "Currently, I am a Rutger's University Full Stack Flex Bootcamp web development student out of the State of New Jersey. Through this course, I've expanded my knowledge on HTML5, CSS, JavaScript (including jQuery and Sequelize), MongoDB, mySQL, Express, React.js, and Node.js. I have developed online and offline apps using these technologies, which are available in my portfolio.", 
 
-    "Due my professional aspirations, I grew enthusiastic on expanding my career spectrum by updating my knowledge in technolgies that I've used both professional and personally. It's my goal to become a team member of a leading organization that's most suited for my goals as a web developer.",
+    "Due to my professional aspirations, I grew enthusiastic on expanding my career spectrum of opportunities by updating my knowledge in technolgies that I've used both professional and personally. It's my goal to become a team member of a leading organization that's most suited for my goals as a web developer.",
 
     "My professional background is pretty diverse, with experience in the transportation and financial indistries. Prior to joining Rutger's University's bootcamp, I had different roles at United Airlines as part of Inflight Services Department, including supervising inflight operations (reports and on the field) and leading a team of more than 200 successful employees. Before working at United, I was part the Organization team at Banco Santander where I had the role of Policies and Procedures Officer.", 
 
