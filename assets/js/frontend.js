a9088360e1ae0327d25c54a86ce35c1f12dff47f
ivanzapatarivera <falenyo@gmail.com>
2020-09-22 00:11:29 -0400

    changed font styles, added signature

--- assets/js/frontend.js
+++ assets/js/frontend.js
@@ -1,39 +1,87 @@
-var briefing = document.getElementById("briefing");
-var title = '<h2 id="title" class="text-center my-3">Welcome to my Profile</h2>'
+const proObj = 
+    {
+        "tag": "img",
+        "className": "profile",
+        "id": "profileImage",
+        "src": "assets/images/profile.png",
+        "alt": "profile picture"
+    }
 
-briefing.innerHTML = title;
+var parsArr = [
 
-var pars = [
+        "Hi, and thank you for taking your time to read a bit about me as a web developer and individual.",
 
-    "Hi, and thank you for taking your time to read a bit about me as a developer and individual.",
+        "Recently, I finished Rutger's University Full Stack Flex web developer certification. Through this course, I've earned knowledge on HTML5, CSS, JavaScript (Vanilla, jQuery and Sequelize), MySql, and MERN (MongoDB, Express, React.js, and Node.js). I've created online and offline apps using these technologies, which are available in my portfolio.", 
 
-    "Currently, I am a Rutger's University Full Stack Flex Bootcamp web development student out of the State of New Jersey. Through this course, I've expanded my knowledge on HTML5, CSS, JavaScript (including jQuery and Sequelize), MongoDB, mySQL, Express, React.js, and Node.js. I have developed online and offline apps using these technologies, which are available in my portfolio.", 
+        "After finishing my certification, I've grown enthusiastic about bringing these skills over and join a leading organization where I can grow as a web developer.",
 
-    "Due to my professional aspirations, I've grown enthusiastic about expanding the spectrum of career opportunities by updating my knowledge in technolgies that I've always been interested in learning.  I plan on bringing these skills over and join a leading organization where I can grow as a web developer.",
+        "My professional background is pretty diverse, with experience in the transportation and financial industries. Prior to joining Rutger's University, I had different roles at United Airlines as part of Inflight Services Department, which included supervising a team of more than 200 employees and helping with the operation. Before being part of United's team, I was part of the Organization Department (under Means) at Banco Santander where I had the role of Policies and Procedures Officer.", 
 
-    "My professional background is pretty diverse, with experience in the transportation and financial indistries. Prior to joining Rutger's University's bootcamp, I had different roles at United Airlines as part of Inflight Services Department, including supervising inflight operations (reports and on the field) and leading a team of more than 200 successful employees. Before working at United, I was part the Organization team at Banco Santander where I had the role of Policies and Procedures Officer.", 
+        "I invite you to visit my GitHub, browse through my profile and see the different projects I've developed along with the team of experts and now graduates from Rutgers.",
 
-    "I invite you to browse through my profile page and see the different projects I've developed along with the team of experts at Rutgers.",
-
-    "Yours truly,"
-
+        "Yours truly,"
 ];
 
-for(var i = 0; i < pars.length; i++) {
-    var elP = document.createElement('p');
-        elP.className = ('p' + [i] + " bodytext");
-        elP.innerHTML = pars[i];
-    briefing.append(elP);
+const signatureObj = {
+    "name": "Iv&aacute;n J. Zapata-Rivera",
+    "className": "signature",
+    "id": "signature"
 }
 
-var lengthP = '"p' + (pars.length - 1) + '"';
-console.log(lengthP)
-var lastP = document.getElementById('"p' + (pars.length - 1) + '"')
+const signatureTitlesObj = {
+        "name": "Iv&aacute;n J. Zapata-Rivera",
+        "certification": "Web Developer Full Stack Flex (Rutgers University)",
+        "mba": "MBA, International Enterprises (Polytechnic University of Puerto Rico)"
+}
 
-var elImg = document.createElement('img');
-    elImg.className = ('signature');
-    elImg.setAttribute = ('src', 'assets/images/signature.png');
-    elImg.setAttribute = ('width', '100px');
-    elImg.setAttribute = ('height', 'auto');
+const cardTitles = {
 
-    lastP.append(elImg);
+}
+
+// Profile Image
+const imgAttrs = proObj
+const imgLoc = document.getElementById('profilePicture');
+
+const img = document.createElement(imgAttrs.tag);
+    img.className = (imgAttrs.className);
+    img.id = (imgAttrs.id);
+    img.src = (imgAttrs.src);
+    img.alt = (imgAttrs.alt);
+    imgLoc.append(img);
+
+// Page title
+const briefing = document.getElementById("briefing");
+const title = '<h2 id="welcome" class="text-center my-3">Welcome to my Profile</h2>'
+    briefing.innerHTML = title;
+
+
+// Summary
+for(var i = 0; i < parsArr.length; i++) {
+    var elP = document.createElement('p');
+        elP.className = ("bodytext mr-2");
+        elP.id = ('p' + [i])
+        elP.innerHTML = parsArr[i];
+        briefing.append(elP);
+}
+
+// Signature
+const   lastP = parsArr.length - 1;
+const   signature = document.createElement("p");
+        signature.className = signatureObj.className;
+        signature.id = signatureObj.id;
+const   signaureText = signatureObj.name;
+        signature.innerHTML = signaureText;
+        briefing.append(signature);
+
+const   createRow = document.createElement("div");
+        createRow.className = "row";
+        createRow.id = "title";
+        briefing.append(createRow);        
+
+const   degreeCertificate = document.getElementById("title");
+const   createCol = document.createElement("div");
+        createCol.className = "col-sm-12 bodytext";
+        createCol.innerHTML = signatureTitlesObj.certification + '</br>' 
+                            + signatureTitlesObj.mba;
+        degreeCertificate.append(createCol);
+
